da95d3f281305d17c045f230b6de9ce023b3aaad
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-02-07 18:46:31 +0300

    22

---
+++ Task_22/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 22. Напишите прогу, которая принимает на вход числ N и выдаёт таблицу квадартов чисел от 1 до N
+5 -> 1,4,9,16,25 */
+
+int a;
+Console.WriteLine($"Введите a: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"{5} -> ");
+for (int i = 1; i <= a; i++)
+{
+    int sum = i * i;
+    Console.Write($"{sum} ");
+}
