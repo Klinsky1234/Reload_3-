5289e4bbac014b2a0f95d1e9b37635c691bf6146
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-02-07 17:12:29 +0300

    18

---
+++ Task_18/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите прогу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти*/
+int x;
+Console.WriteLine($"Введите номер четверти");
+int.TryParse(Console.ReadLine()!, out x);
+
+if (x == 1)
+    Console.WriteLine($"X > 0 Y > 0");
+else if (x == 2)
+    Console.WriteLine($"X < 0 Y > 0");
+else if (x == 3)
+    Console.WriteLine($"X < 0 Y < 0");
+else if (x == 4)
+    Console.WriteLine($"X > 0 Y < 0");
+else
+    Console.WriteLine($"Неверный ввод");
